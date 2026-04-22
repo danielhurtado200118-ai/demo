e5e00d4e44c4869e915c612401d6418438a232fb
danielhurtado200118-ai <danielhurtado200118@gmail.com>
2026-04-22 14:26:10 -0600

    Update SecurityConfig.java

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -17,7 +17,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // CUMPLE RF-02: BCrypt factor 12
         return new BCryptPasswordEncoder(12);
     }
 
@@ -32,20 +31,10 @@ public class SecurityConfig {
                 config.setAllowedHeaders(List.of("*"));
                 return config;
             }))
-            .headers(headers -> headers
-                .contentSecurityPolicy(csp -> csp.policyDirectives("default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"))
-                .frameOptions(f -> f.deny())
-            )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/index.html", "/api/usuarios/login").permitAll()
-                
-                // RBAC: Roles según tu HTML
                 .requestMatchers("/api/usuarios/**").hasAnyAuthority("SUPERADMIN", "ADMIN")
                 .requestMatchers("/api/productos/**").hasAnyAuthority("SUPERADMIN", "ADMIN", "REGISTRADOR")
-                
-                // Permitir que el Auditor lea (GET) pero no modifique
-                .requestMatchers("/api/**").hasAuthority("AUDITOR")
-                
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults());
